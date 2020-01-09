66ae212b98954ef4c1cc029bc7658e3bf13a83e2
Jacob Pierce <w.jacobpierce@gmail.com>
2020-01-09 15:37:33 -0800

    Hello world

--- kolibri/plugins/exercise_qti_renderer/assets/src/module.js
+++ kolibri/plugins/exercise_qti_renderer/assets/src/module.js
@@ -3,8 +3,8 @@ import ContentRendererModule from 'content_renderer_module';
 
 class QtiExerciseRendererModule extends ContentRendererModule {
   get rendererComponent() {
-    ExerciseQTIRenderer.contentModule = this;
-    return ExerciseQTIRenderer;
+    QtiExerciseRenderer.contentModule = this;
+    return QtiExerciseRenderer;
   }
 }
 
