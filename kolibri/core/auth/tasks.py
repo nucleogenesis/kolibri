cf8af5e97ac9236c7921653c551b1d5a34d528cd
Jacob Pierce <w.jacobpierce@gmail.com>
2025-03-21 12:52:31 -0700

    ImportUsersFromCSVValidator: Use default storage for uploaded file

--- kolibri/core/auth/tasks.py
+++ kolibri/core/auth/tasks.py
@@ -1,9 +1,8 @@
 import logging
 import ntpath
-import os
-import shutil
 
 from django.conf import settings
+from django.core.files.storage import default_storage
 from django.core.management import call_command
 from django.utils import timezone
 from rest_framework import serializers
@@ -41,8 +40,6 @@ from kolibri.core.tasks.permissions import IsSuperAdmin
 from kolibri.core.tasks.permissions import NotProvisioned
 from kolibri.core.tasks.utils import get_current_job
 from kolibri.core.tasks.validation import JobValidator
-from kolibri.utils.conf import KOLIBRI_HOME
-from kolibri.utils.filesystem import mkdirp
 from kolibri.utils.time_utils import naive_utc_datetime
 from kolibri.utils.translation import gettext as _
 
@@ -126,6 +123,7 @@ class ImportUsersFromCSVValidator(JobValidator):
             raise serializers.ValidationError(
                 "One of csvfile or csvfilename must be specified"
             )
+
         facility = data.get("facility")
         if facility:
             facility_id = facility.id
@@ -133,17 +131,16 @@ class ImportUsersFromCSVValidator(JobValidator):
             facility_id = self.context["user"].facility_id
         else:
             raise serializers.ValidationError("Facility must be specified")
-        temp_dir = os.path.join(KOLIBRI_HOME, "temp")
-        mkdirp(temp_dir, exist_ok=True)
+
         if "csvfile" in data:
-            tmpfile = data["csvfile"].temporary_file_path()
-            filename = ntpath.basename(tmpfile)
-            filepath = os.path.join(temp_dir, filename)
-            shutil.copyfile(tmpfile, filepath)
+            tmp_path = data["csvfile"].temporary_file_path()
+            filename = ntpath.basename(tmp_path)
+            filepath = default_storage.save(filename, data["csvfile"])
         else:
-            filepath = os.path.join(temp_dir, data["csvfilename"])
-            if not os.path.exists(filepath):
+            filepath = data["csvfilename"]
+            if not default_storage.exists(filepath):
                 raise serializers.ValidationError("Supplied csvfilename does not exist")
+
         args = [filepath]
         kwargs = {
             "locale": data.get("locale"),
